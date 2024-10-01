a50a55e36203d0fa2615424df18910be7c02d891
gaetanovenuto <gaetanovenuto99@live.it>
2024-10-01 13:06:10 +0200

    Creato Template

---
+++ BackEnd/api.php
@@ -0,0 +1,9 @@
+<?php
+
+/* PER IL FUTURO */
+// header("Access-Control-Allow-Origin: http://127.0.0.1:5173");
+// header("Access-Control-Allow-Headers: X-Requested-With");
+	
+header('Content-Type: application/json');
+ 
+echo json_encode($students);
